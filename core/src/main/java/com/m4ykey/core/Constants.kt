cce0e0674b9dc2d3cc79468371353195efb1e0fd
m4ykey <kfjdjdkh@gmail.com>
2024-02-05 20:48:42 +0100

    several new news services have been added

--- core/src/main/java/com/m4ykey/core/Constants.kt
+++ core/src/main/java/com/m4ykey/core/Constants.kt
@@ -6,10 +6,16 @@ object Constants {
 
     const val NEWS_BASE_URL = "https://newsapi.org/"
     const val DOMAINS =
-        "${MusicSources.ROLLING_STONE}, ${MusicSources.BILLBOARD}, " + MusicSources.PITCHFORK
+        "${MusicSources.ROLLING_STONE}, ${MusicSources.BILLBOARD}, ${MusicSources.PITCHFORK}, ${MusicSources.INTERIA}, ${MusicSources.NME}, " +
+                "${MusicSources.CONSEQUENCE}, ${MusicSources.STEREOGUM}, ${MusicSources.THE_FADER}"
     const val ROLLING_STONE_LOGO = "https://play-lh.googleusercontent.com/ToohFdJJ016UxyObeFjWkB6wtp3_M-DiSaPvCJqV19kU_k0mGZ7SJPKrodbNJw4KGT4=w240-h480-rw"
     const val PITCHFORK_LOGO = "https://yt3.googleusercontent.com/ytc/AIf8zZS9edaTMwek-1vVJgY6yP5mMXfzQo0nyvnmD4arHQ=s900-c-k-c0x00ffffff-no-rj"
     const val BILLBOARD_LOGO = "https://upload.wikimedia.org/wikipedia/commons/0/0d/Billboard-logo-web.png"
+    const val INTERIA_LOGO = "https://firma.interia.pl/static/caf57bd01b7d42c6bc1d783e83a75d15/5bec7/Component-35-E28093-1.png"
+    const val STEREOGUM_LOGO = "https://pbs.twimg.com/profile_images/1325696974578913280/NRN47Aw-_400x400.jpg"
+    const val NME_LOGO = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/NME_logo_free.svg/2560px-NME_logo_free.svg.png"
+    const val CONSEQUENCE_LOGO = "https://upload.wikimedia.org/wikipedia/en/9/95/Consequence-main-logo.jpg"
+    const val THE_FADER_LOGO = "https://www.thefader.com/assets/Fader-New-Black-FB-d2aaa59a0df7666f2ffae62bf70a70896fa5d60656f2c33d97f9ce70d98a2209.png"
 
     const val SPOTIFY_AUTH_URL = "https://accounts.spotify.com/"
     const val SPOTIFY_BASE_URL = "https://api.spotify.com/v1/"
@@ -32,4 +38,9 @@ object MusicSources {
     const val ROLLING_STONE = "rollingstone.com"
     const val BILLBOARD = "billboard.com"
     const val PITCHFORK = "pitchfork.com"
+    const val NME = "nme.com"
+    const val CONSEQUENCE = "consequence.net"
+    const val STEREOGUM = "stereogum.com"
+    const val THE_FADER = "thefader.com"
+    const val INTERIA = "interia.pl"
 }
--- news/ui/src/main/java/com/m4ykey/ui/components/NewsCard.kt
+++ news/ui/src/main/java/com/m4ykey/ui/components/NewsCard.kt
@@ -25,8 +25,13 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.m4ykey.core.Constants.BILLBOARD_LOGO
+import com.m4ykey.core.Constants.CONSEQUENCE_LOGO
+import com.m4ykey.core.Constants.INTERIA_LOGO
+import com.m4ykey.core.Constants.NME_LOGO
 import com.m4ykey.core.Constants.PITCHFORK_LOGO
 import com.m4ykey.core.Constants.ROLLING_STONE_LOGO
+import com.m4ykey.core.Constants.STEREOGUM_LOGO
+import com.m4ykey.core.Constants.THE_FADER_LOGO
 import com.m4ykey.core.composable.LoadImage
 import com.m4ykey.core.composable.StyledText
 import com.m4ykey.core.helpers.formatDate
@@ -47,7 +52,12 @@ fun NewsCard(
     val logos = mapOf(
         "Rolling Stone" to ROLLING_STONE_LOGO,
         "Pitchfork" to PITCHFORK_LOGO,
-        "Billboard" to BILLBOARD_LOGO
+        "Billboard" to BILLBOARD_LOGO,
+        "Interia.pl" to INTERIA_LOGO,
+        "Stereogum" to STEREOGUM_LOGO,
+        "NME" to NME_LOGO,
+        "Consequence.net" to CONSEQUENCE_LOGO,
+        "The FADER" to THE_FADER_LOGO
     )
 
     val formattedDate = formatDate(
